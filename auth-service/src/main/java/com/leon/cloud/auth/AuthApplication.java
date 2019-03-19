b9e95d05c5ec2c9420f1462eec3ffe23e9b25233
liyang <cp3leon@gmail.com>
2019-03-19 12:27:12 +0800

    profiles modify

---
+++ auth-service/src/main/java/com/leon/cloud/auth/AuthApplication.java
@@ -0,0 +1,16 @@
+package com.leon.cloud.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by leon on 2019/3/19.
+ */
+
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
