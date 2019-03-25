e22259d5c806f9a83837bf44057083671dfab960
liyang <cp3leon@gmail.com>
2019-03-25 17:41:17 +0800

    add docker-compose wait-for-it

---
+++ registry/src/main/java/com/leon/cloud/registry/RegistryApplication.java
@@ -0,0 +1,18 @@
+package com.leon.cloud.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by leon on 2019/3/5.
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}
