c75feacc9e0e0f639336ebd14299a75b474cc136
liyang <cp3leon@gmail.com>
2019-03-26 09:29:59 +0800

    add docker-compose wait-for-it

--- registry/startup.sh
+++ registry/startup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+echo -Xmx200m -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=$1 -jar /app/$2.jar
 java -Xmx200m -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=$1 -jar /app/$2.jar
