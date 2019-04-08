38fa03a7c9ad35d6b84b1f5972106b80b70b99c5
liyang <liyang@shpso.com>
2019-04-08 12:19:53 +0800

    audit modules

--- service/audit-service/src/main/java/com/leon/cloud/audit/AuditApplication.java
+++ service/audit-service/src/main/java/com/leon/cloud/audit/AuditApplication.java
@@ -4,10 +4,12 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableScheduling
 @Log4j2
 public class AuditApplication {
 
