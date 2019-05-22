32b43500726fce1c46cdc3f956a913fa1e171737
liyang <liyang@shpso.com>
2019-05-22 10:35:27 +0800

    add note

--- cloud-common/src/main/java/com/leon/cloud/common/concurrent/semaphore/SemaphoreExample.java
+++ cloud-common/src/main/java/com/leon/cloud/common/concurrent/semaphore/SemaphoreExample.java
@@ -4,14 +4,19 @@ public class SemaphoreExample {
 
 
     public static void main(String[] args) throws InterruptedException {
-        int size=10;
-        Object[] items=new Object[size];
-        for(int i=0;i<size;i++){
-            items[i]=new Book(i);
+        int size = 10;
+        Object[] items = new Object[size];
+        for (int i = 0; i < size; i++) {
+            items[i] = new Book(i);
         }
-        Pool pool=new Pool(items);
-        for(int i=0;i<11;i++){
+        Pool pool = new Pool(items);
+        Object o;
+        for (int i = 0; i < size - 1; i++) {
             System.out.println(pool.getItem());
         }
+        o=pool.getItem();
+        pool.putItem(o);
+
+        System.out.println(pool.getItem());
     }
 }
